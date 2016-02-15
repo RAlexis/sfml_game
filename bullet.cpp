2706b7faf116d573e8632c954bf3bc9c4ae9232e
Razi Alexis <razialexis43@gmail.com>
2016-02-15 19:38:17 +0200

    Removed some code that has been rewritten

--- bullet.cpp
+++ bullet.cpp
@@ -1,58 +1,5 @@
 #include "bullet.h"
 #include <iostream>
-/*
-
-const float Bullet::lifetime = 1000.0f;
-const float Bullet::speed = 0.3f;
-
-Bullet::Bullet(sf::Vector2f position)
-{
-    this->is_alive = true;
-    this->remaining_life = lifetime;
-    this->direction = sf::Vector2f(0, -1); // upwards
-
-    if (bulletTexture.loadFromFile("bullet.png"))
-        bulletSprite.setTexture(bulletTexture);
-    else
-        std::cout << "[ERROR] Could not load bullet.png." << std::endl;
-
-    bulletSprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
-
-    //bulletSprite.setTexture(bulletTexture);
-    bulletSprite.setPosition(position.x + 16, position.y);
-    //this->setPosition(sf::Vector2f(position.x + 16, position.y));
-
-}
-
-Bullet::~Bullet()
-{
-
-}
-
-bool Bullet::isAlive()
-{
-    return is_alive;
-}
-
-void Bullet::update(float frametime)
-{
-    if (!is_alive)
-        return;
-
-    remaining_life -= frametime;
-
-    if (remaining_life <= 0)
-        is_alive = false;
-    
-    sf::Vector2f distance = direction * speed * frametime;
-    bulletSprite.move(distance);
-}
-
-void Bullet::kill()
-{
-    is_alive = false;
-}
-*/
 
 const float Bullet::lifetime = 1000.0f;
 const float Bullet::speed = 0.3f;
--- bullet.h
+++ bullet.h
@@ -4,46 +4,6 @@
 #include <SFML/Graphics.hpp>
 #include "gamedefines.h"
 
-/*
-class Bullet
-{
-    static const float lifetime;
-    static const float speed;
-
-public:
-    Bullet(sf::Vector2f position);
-    ~Bullet();
-
-    bool isAlive();
-    void kill();
-    void update(float frametime);
-
-    void setAlreadyFired(bool val)
-    {
-        this->alreadyFired = val;
-    }
-
-    bool hasAlreadyFired()
-    {
-        return alreadyFired;
-    }
-
-    sf::Sprite getSprite()
-    {
-        return this->bulletSprite;
-    }
-
-    sf::Texture bulletTexture;
-    sf::Sprite bulletSprite;
-
-private:
-    bool is_alive;
-    float remaining_life;
-    sf::Vector2f direction;
-    bool alreadyFired;
-};
-*/
-
 class Bullet
 {
     static const float lifetime;
